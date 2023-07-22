b0e1c69953393314809b9e364b7bc553aa2bb00d
ahbab259 <hasin_ahbab@yahoo.com>
2023-07-22 16:56:04 -0400

    First Commit

---
+++ src/Organization.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export class Organization extends Component{
+    render(){
+        return(
+            <div>
+                <h3>This is Organizations Page</h3>
+            </div>
+        )
+    }
+}
---
+++ src/Project.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export class Project extends Component{
+    render(){
+        return(
+            <div>
+                <h3>This is Projects Page</h3>
+            </div>
+        )
+    }
+}
